37a7fb8b732181ba540021fd55dbc3e8cb35a503
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-20 23:34:40 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -7,18 +7,17 @@ def apply_coupons(cart, coupons)
   # REMEMBER: This method **should** update cart
   index = 0
   coupons.each do |coupon|
-    grocery_with_coupon = find_item_by_name_in_collection(coupon[:item],cart)
-    grocery_in_card = grocery_with_coupon
-    count_to_apply = grocery_in_card && grocery_with_coupon[:count] >= coupon[:num]
-    if grocery_in_card && count_to_apply
-      cart << {item: "#{grocery_with_coupon[:item]} W/COUPON", price: coupon[:cost]/coupon[:num],
-      clearence: grocery_with_coupon[:clearence], count: coupon[:num]} 
+    grocery_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
+    grocery_in_cart = grocery_with_coupon
+    coupon_to_apply = grocery_in_cart && grocery_with_coupon[:count] >= coupon[:num]
+    if grocery_in_cart && coupon_to_apply
+      cart << {item: "#{grocery_with_coupon[:item]} W/COUPON",price: coupon[:cost] / coupon[:num],
+      clearance: grocery_with_coupon[:clearance], count: coupon[:num]}
       grocery_with_coupon[:count] -= coupon[:num]
-    
     end
-   index += 1
- end
- cart
+    index += 1
+  end
+  cart
   end
 
 def apply_clearance(cart)
@@ -28,8 +27,7 @@ def apply_clearance(cart)
   
   cart.each do |grocery|
     if grocery[:clearance]
-      grocery[:price] *= 0.80
-      
+      grocery[:price] *= 0.8
     end
     grocery
   end
@@ -46,12 +44,13 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   
-  final_cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+   final_cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  
   total = 0
+
   final_cart.each do |grocery|
     total += grocery[:price] * grocery[:count]
   end
-  total *= 0.9 if total > 100
-  total.round(2)
-  
+total *= 0.9 if total > 100
+total.round(2)
 end
